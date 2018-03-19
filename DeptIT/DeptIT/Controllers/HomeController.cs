bf2c39463c6c99cb61b0857a67c8325838546066
salex42 <lexgml@gmail.com>
2018-03-19 15:43:29 +0700

    First commit

---
+++ DeptIT/DeptIT/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DeptIT.Models;
+
+namespace DeptIT.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        DBContext db = new DBContext();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var products = db.Products;
+            ViewBag.Products = products;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.ProductId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            order.OrderDate = DateTime.Now;
+            // добавляем информацию о покупке в базу данных
+            db.Orders.Add(order);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо," + order.Client + ", за покупку!";
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DeptIT/DeptIT/Models/DBContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DeptIT.Models
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+
+    class RequestInitializer : DropCreateDatabaseAlways<DBContext>
+    {
+        protected override void Seed(DBContext db)
+        {
+            db.Products.Add(new Product { Id = 1, Name = "Колье", Cost = 2500, Type = "Украшение" });
+            db.Products.Add(new Product { Id = 2, Name = "Телевизор", Cost = 25000, Type = "Электроника" });
+            db.Products.Add(new Product { Id = 3, Name = "Брош", Cost = 500, Type = "Украшение" });
+
+            base.Seed(db);
+        }
+    }
+}
