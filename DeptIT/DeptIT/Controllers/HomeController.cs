cda533f8dbff24e21cb1ce40072c120602140d2b
salex42 <lexgml@gmail.com>
2018-03-23 17:22:19 +0700

    1

--- DeptIT/DeptIT/Controllers/HomeController.cs
+++ DeptIT/DeptIT/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DeptIT.Models;
+using DeptIT.Utils;
 
 namespace DeptIT.Controllers
 {
@@ -11,13 +12,32 @@ namespace DeptIT.Controllers
     {
 
         DBContext db = new DBContext();
+        public int Orders_cnt = 0;
 
         [HttpGet]
         public ActionResult Index()
         {
-            var products = db.Products;
+            IEnumerable<Product> products = db.Products;
+            string client = "";
+            try
+            {
+                client = HttpContext.Request.Cookies["client"].Value;
+            }
+            catch
+            { }
+
+            Session["login"] = "salex42";
             ViewBag.Products = products;
+            ViewBag.Client = client;
+            ViewData["cnt"] = Orders_cnt;
             return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Products()
+        {
+            return View(db.Products);
         }
 
         [HttpGet]
@@ -28,11 +48,13 @@ namespace DeptIT.Controllers
         }
 
         [HttpPost]
-        public string Buy(Order order)
+        public string Buy(Ordert order)
         {
-            order.OrderDate = DateTime.Now;
+            //order.OrderDate = DateTime.Now;
             // добавляем информацию о покупке в базу данных
+            HttpContext.Response.Cookies["client"].Value = order.Client;
             db.Orders.Add(order);
+            Orders_cnt++;
             // сохраняем в бд все изменения
             db.SaveChanges();
             return "Спасибо," + order.Client + ", за покупку!";
@@ -41,8 +63,17 @@ namespace DeptIT.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            return new HttpStatusCodeResult(404);
 
             return View();
+        }
+
+        [HttpGet]
+        public HtmlResult Square(int a = 2, int b = 4)
+        {
+            double square;
+            square = a * b / (double)2;
+            return new HtmlResult(String.Format("<h2> Площадь треугольника: {0:0.###} </h2>", square));
         }
 
         public ActionResult Contact()
@@ -50,6 +81,59 @@ namespace DeptIT.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public FileResult GetFile()
+        {
+            // Путь к файлу
+            string file_path = Server.MapPath("~/Files/1EM.pdf");
+            // Тип файла - content-type
+            string file_type = "application/pdf";
+            // Имя файла - необязательно
+            string file_name = "1EM.pdf";
+            return File(file_path, file_type, file_name);
+        }
+
+        public string Context()
+        {
+            HttpContext.Response.Write("<h1>Hello World</h1>");
+            HttpContext.Response.Write("<h1>Hello World, login " + Session["login"] + "</h1>");
+            string browser = HttpContext.Request.Browser.Browser;
+            string user_agent = HttpContext.Request.UserAgent;
+            string url = HttpContext.Request.RawUrl;
+            string ip = HttpContext.Request.UserHostAddress;
+            string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
+            return "<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
+                "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>";
+        }
+
+
+        [HttpGet]
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SearchProduct(string name)
+        {
+            var allproducts = db.Products.Where(a => a.Name.Contains(name)).ToList();
+            if (allproducts.Count <= 0)
+            {
+                return PartialView();
+            }
+            return PartialView(allproducts);
+        }
+
+        [HttpGet]
+        public ActionResult Detail(int id)
+        {
+            Product prod = db.Products.FirstOrDefault(a => a.Id == id);
+            if (prod != null)
+            {
+                return PartialView(prod);
+            }
+            return HttpNotFound();
         }
     }
 }
--- DeptIT/DeptIT/Models/DBContext.cs
+++ DeptIT/DeptIT/Models/DBContext.cs
@@ -9,7 +9,7 @@ namespace DeptIT.Models
     public class DBContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
-        public DbSet<Order> Orders { get; set; }
+        public DbSet<Ordert> Orders { get; set; }
     }
 
     class RequestInitializer : DropCreateDatabaseAlways<DBContext>
@@ -19,6 +19,13 @@ namespace DeptIT.Models
             db.Products.Add(new Product { Id = 1, Name = "Колье", Cost = 2500, Type = "Украшение" });
             db.Products.Add(new Product { Id = 2, Name = "Телевизор", Cost = 25000, Type = "Электроника" });
             db.Products.Add(new Product { Id = 3, Name = "Брош", Cost = 500, Type = "Украшение" });
+            db.Products.Add(new Product { Id = 4, Name = "Брошка 1", Cost = 500, Type = "Украшение" });
+            db.Products.Add(new Product { Id = 5, Name = "Брошка 12", Cost = 500, Type = "Украшение" });
+            db.Products.Add(new Product { Id = 6, Name = "Брошка 13", Cost = 500, Type = "Украшение" });
+            db.Products.Add(new Product { Id = 7, Name = "Брошка 111", Cost = 500, Type = "Украшение" });
+            db.Products.Add(new Product { Id = 8, Name = "Брошка 1768", Cost = 500, Type = "Украшение" });
+
+            db.Orders.Add(new Ordert { OrdertId = 1, Client = "Ivanov", ProductId = 1, Count = 2 });
 
             base.Seed(db);
         }
